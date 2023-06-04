0e839b063b87a62b76f30412c0fbb7d44f0568de
ag7621 <aaron.c.guerra@gmail.com>
2023-06-04 16:31:01 -0700

    update index, style, app
    added basic js, styled both mobile/desktop social links sections

---
+++ app.js
@@ -0,0 +1,11 @@
+const cardFooter = document.querySelector('.card__footer');
+const authorInfo = document.querySelector('.card__author');
+const socialMedia = document.querySelector('.card__social-links');
+
+const socialBtn = document.querySelector('.social-links');
+
+socialBtn.addEventListener('click', () => {
+    cardFooter.classList.toggle('card__footer-active')
+    authorInfo.classList.toggle('disabled')
+    socialMedia.classList.toggle('disabled')
+})
